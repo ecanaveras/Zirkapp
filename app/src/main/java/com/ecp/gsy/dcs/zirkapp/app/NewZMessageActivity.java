56f460e8a09179bf860ec921d5b79c1e6d1e49a7
ecanaveras <elderpianeta@gmail.com>
2014-07-21 18:21:59 -0500

    Update 21072014 1821

--- app/src/main/java/com/ecp/gsy/dcs/zirkapp/app/NewZMessageActivity.java
+++ app/src/main/java/com/ecp/gsy/dcs/zirkapp/app/NewZMessageActivity.java
@@ -18,7 +18,7 @@ public class NewZMessageActivity extends Activity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.messages_activity_action, menu);
+        //getMenuInflater().inflate(R.menu.messages_activity_action, menu);
         return true;
     }
 
