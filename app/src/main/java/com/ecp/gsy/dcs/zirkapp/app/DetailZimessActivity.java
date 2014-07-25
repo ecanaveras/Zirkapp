1a5fa18f6dac87a28b1b2bf83dbb607fb2c59529
ecanaveras <elderpianeta@gmail.com>
2014-07-25 13:30:39 -0500

    Update 25072014 1330

---
+++ app/src/main/java/com/ecp/gsy/dcs/zirkapp/app/DetailZimessActivity.java
@@ -0,0 +1,40 @@
+package com.ecp.gsy.dcs.zirkapp.app;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import com.ecp.gsy.dcs.zirkapp.app.R;
+import com.ecp.gsy.dcs.zirkapp.app.util.beans.Zimess;
+
+public class DetailZimessActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail_zimess);
+        //getIntent().getStringExtra();
+        getActionBar().setDisplayHomeAsUpEnabled(true);
+        getActionBar().setTitle("Zimess");
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.detail_zimess, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
