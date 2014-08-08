fb931c46d4fd1909d00e7f02d196109b1d6c1196
ecanaveras <Elderca1986>
2014-08-08 18:00:54 -0500

    Add AlertDialog Config Home Update

---
+++ app/src/main/java/com/ecp/gsy/dcs/zirkapp/app/util/dialog/EditDistanceDialog.java
@@ -0,0 +1,93 @@
+package com.ecp.gsy.dcs.zirkapp.app.util.dialog;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.NumberPicker;
+import android.widget.Toast;
+
+import com.ecp.gsy.dcs.zirkapp.app.R;
+
+/**
+ * Created by elcapi05 on 08/08/2014.
+ */
+public class EditDistanceDialog extends DialogFragment {
+
+    private NumberPicker distMinPicker;
+    private NumberPicker distMaxPicker;
+    private int distanceMin;
+    private int distanceMax;
+
+    public interface EditDistanceDialogListener {
+        public void onDialogPositiveClick(EditDistanceDialog dialogFragment);
+
+        public void onDialogNegativeClick(EditDistanceDialog dialogFragment);
+    }
+
+    EditDistanceDialogListener listener;
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        LayoutInflater layoutInflater = LayoutInflater.from(getActivity());
+        View vConfig = (View) layoutInflater.inflate(R.layout.layout_edit_distance, null);
+        //Conf picker
+        distMinPicker = (NumberPicker) vConfig.findViewById(R.id.pickerDistMin);
+        distMaxPicker = (NumberPicker) vConfig.findViewById(R.id.pickerDistMax);
+        distMinPicker.setMinValue(10);
+        distMinPicker.setMaxValue(500);
+        distMaxPicker.setMinValue(1000);
+        distMaxPicker.setMaxValue(5000);
+        distMaxPicker.setWrapSelectorWheel(true);
+        distMaxPicker.setWeightSum(1000);
+        AlertDialog.Builder alBuilder = new AlertDialog.Builder(getActivity());
+        alBuilder.setView(vConfig);
+        alBuilder.setTitle(R.string.lblConfigDistance);
+        alBuilder
+                .setCancelable(false)
+                .setPositiveButton(R.string.lblOk, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        if (dialogInterface != null) {
+                            Dialog dialog = Dialog.class.cast(dialogInterface);
+                            distMinPicker = (NumberPicker) dialog.findViewById(R.id.pickerDistMin);
+                            distMaxPicker = (NumberPicker) dialog.findViewById(R.id.pickerDistMax);
+                            distanceMin = distMinPicker.getValue();
+                            distanceMax = distMaxPicker.getValue();
+                            dialog.dismiss();
+                        }
+                    }
+                })
+                .setNegativeButton(R.string.lblCancel, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        dialogInterface.cancel();
+                    }
+                });
+
+        return alBuilder.create();
+    }
+
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+        try {
+            listener = (EditDistanceDialogListener) activity;
+        } catch (ClassCastException e) {
+            throw new ClassCastException(activity.toString() + " no implementa EditDistanceDialogListener");
+        }
+
+    }
+
+    public int getDistanceMin() {
+        return distanceMin;
+    }
+
+    public int getDistanceMax() {
+        return distanceMax;
+    }
+}
