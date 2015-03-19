f5dbbf7637f06beff656a7db5632d64db6c68c2e
ecanaveras <elderpianeta@gmail.com>
2015-03-19 02:18:04 -0500

    Agregada la busqueda de usuarios online y dentro del rango en kms, ajustado el contador del drawer, ajustado el layout de los usuarios

---
+++ app/src/main/java/com/ecp/gsy/dcs/zirkapp/app/util/adapters/UsersAdapter.java
@@ -0,0 +1,73 @@
+package com.ecp.gsy.dcs.zirkapp.app.util.adapters;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.ecp.gsy.dcs.zirkapp.app.R;
+import com.ecp.gsy.dcs.zirkapp.app.util.task.GlobalApplication;
+import com.parse.ParseException;
+import com.parse.ParseUser;
+
+import java.util.List;
+
+/**
+ * Created by Elder on 18/03/2015.
+ */
+public class UsersAdapter extends BaseAdapter {
+
+    private List<ParseUser> parseUserList;
+    private Context context;
+
+    public UsersAdapter(Context context, List<ParseUser> parseUserList) {
+        this.context = context;
+        this.parseUserList = parseUserList;
+    }
+
+    @Override
+    public int getCount() {
+        return parseUserList.size();
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return parseUserList.get(i);
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return i;
+    }
+
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+        View vista = view;
+        if (vista == null) {
+            LayoutInflater layoutInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            vista = layoutInflater.inflate(R.layout.listview_item_users, viewGroup, false);
+        }
+
+        //1. Tomar usuario
+        ParseUser parseUser = parseUserList.get(i);
+        //2. Iniciar UI de la lista
+        ImageView imgAvatar = (ImageView) vista.findViewById(R.id.imgAvatar);
+        TextView lblCommentUser = (TextView) vista.findViewById(R.id.lblUserName);
+        TextView lblCommentName = (TextView) vista.findViewById(R.id.lblNombreUsuario);
+
+        //3. Asignar valores
+        lblCommentUser.setText(parseUser.getUsername());
+        lblCommentName.setText(parseUser.getString("name"));
+        //Estableciendo Imagen;
+        imgAvatar.setImageBitmap(GlobalApplication.getAvatar(parseUser));
+
+        return vista;
+    }
+
+}
