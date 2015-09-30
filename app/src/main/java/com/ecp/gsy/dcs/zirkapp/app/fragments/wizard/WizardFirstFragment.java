42f032aeebae3f650f10df14470f194e27be9be1
ecanaveras <elderpianeta@gmail.com>
2015-09-29 20:49:28 -0500

    Upgrade Sept 005

--- app/src/main/java/com/ecp/gsy/dcs/zirkapp/app/fragments/wizard/WizardFirstFragment.java
+++ app/src/main/java/com/ecp/gsy/dcs/zirkapp/app/fragments/wizard/WizardFirstFragment.java
@@ -5,17 +5,18 @@ import android.app.Dialog;
 import android.app.DialogFragment;
 import android.app.Fragment;
 import android.os.Bundle;
+import android.util.Patterns;
 import android.view.LayoutInflater;
-import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
-import android.widget.TextView;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
 
 import com.ecp.gsy.dcs.zirkapp.app.R;
-import com.ecp.gsy.dcs.zirkapp.app.activities.ManagerLogin;
 import com.ecp.gsy.dcs.zirkapp.app.activities.ManagerWizard;
 import com.parse.ParseUser;
 
@@ -29,11 +30,13 @@ import java.util.Date;
  */
 public class WizardFirstFragment extends Fragment {
 
-    private EditText lblUser;
-    private EditText lblUserNombre;
-    private EditText lblEmail;
-    private static EditText lblFecha;
+    private EditText txtUsername;
+    private EditText txtNombres;
+    private EditText txtEmail;
+    private RadioGroup radioGroup;
+    private static EditText txtFecha;
     private ParseUser currentUser;
+    private RadioButton rbtnM, rbtnF;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -49,19 +52,23 @@ public class WizardFirstFragment extends Fragment {
     }
 
     private void inicializarCompUI(View view) {
-        lblUser = (EditText) view.findViewById(R.id.txtUsername);
-        lblUserNombre = (EditText) view.findViewById(R.id.txtUserNombres);
-        lblEmail = (EditText) view.findViewById(R.id.txtUserEmail);
-        lblFecha = (EditText) view.findViewById(R.id.txtUserEdad);
+        txtUsername = (EditText) view.findViewById(R.id.txtUsername);
+        txtNombres = (EditText) view.findViewById(R.id.txtUserNombres);
+        txtEmail = (EditText) view.findViewById(R.id.txtUserEmail);
+        txtFecha = (EditText) view.findViewById(R.id.txtUserBirthday);
+        radioGroup = (RadioGroup) view.findViewById(R.id.radioG);
+        rbtnF = (RadioButton) view.findViewById(R.id.rbtnFemenino);
+        rbtnM = (RadioButton) view.findViewById(R.id.rbtnMasculino);
 
-        lblFecha.setOnClickListener(new View.OnClickListener() {
+
+        txtFecha.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View view) {
                 DatePickerFragment newFragment = null;
-                if (lblFecha.getText() != null && !lblFecha.getText().toString().isEmpty()) {
+                if (txtFecha.getText() != null && !txtFecha.getText().toString().isEmpty()) {
                     try {
-                        newFragment = new DatePickerFragment(new SimpleDateFormat("dd/MM/yyyy").parse(lblFecha.getText().toString().trim()));
+                        newFragment = new DatePickerFragment(new SimpleDateFormat("dd/MM/yyyy").parse(txtFecha.getText().toString().trim()));
                     } catch (ParseException e) {
                         e.printStackTrace();
                     }
@@ -80,21 +87,85 @@ public class WizardFirstFragment extends Fragment {
         btnNext.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //Cambia el fragment
-                ManagerWizard managerWizard = (ManagerWizard) getActivity();
-                managerWizard.mViewPager.setCurrentItem(managerWizard.mViewPager.getCurrentItem() + 1);
+                if (saveDataUser()) {
+                    //Cambia el fragment
+                    ManagerWizard managerWizard = (ManagerWizard) getActivity();
+                    managerWizard.mViewPager.setCurrentItem(managerWizard.mViewPager.getCurrentItem() + 1);
+                }
             }
         });
     }
 
+    private boolean saveDataUser() {
+        if (currentUser != null) {
+            String correo = txtEmail.getText().toString();
+            String username = txtUsername.getText().toString().trim();
+            String nombres = txtNombres.getText().toString().trim();
+            String fecNac = txtFecha.getText().toString().trim();
+
+
+            //Validacion Basica -Deben estar los datos diligenciados-
+            int error = 0;
+
+            if (username.isEmpty()) {
+                txtUsername.setError(getResources().getString(R.string.msgSignUpUserEmpty));
+                error++;
+            }
+
+            if (correo.isEmpty()) {
+                txtEmail.setError(getResources().getString(R.string.msgSignUpEmailEmpty));
+                error++;
+            } else if (!Patterns.EMAIL_ADDRESS.matcher(correo).matches()) {
+                txtEmail.setError(getResources().getString(R.string.msgEmailInvalid));
+                error++;
+            }
+            if (nombres.isEmpty()) {
+                txtNombres.setError(getResources().getString(R.string.msgProfiNameEmty));
+                error++;
+            }
+            if (fecNac.isEmpty()) {
+                txtFecha.setError(getResources().getString(R.string.msgProfiBirthdayEmty));
+                error++;
+            }
+
+            if (error != 0) {
+                return false;
+            }
+
+            Calendar c = Calendar.getInstance();
+            try {
+                c.setTime(new SimpleDateFormat("dd/MM/yyyy").parse(txtFecha.getText().toString().trim()));
+            } catch (ParseException e) {
+                e.printStackTrace();
+            }
+
+            currentUser.setEmail(correo);
+            currentUser.put("name", nombres);
+            currentUser.put("birthday", c.getTime());
+            currentUser.put("gender", (radioGroup.getCheckedRadioButtonId() == R.id.rbtnMasculino ? "M" : "F"));
+
+            currentUser.saveInBackground();
+        }
+
+        return true;
+
+    }
 
     private void loadDataUser() {
         if (currentUser != null) {
-            lblUser.setText(currentUser.getUsername());
-            lblEmail.setText(currentUser.getString("email"));
-            lblUserNombre.setText(currentUser.getString("name"));
+            txtUsername.setText(currentUser.getUsername());
+            txtEmail.setText(currentUser.getString("email"));
+            txtNombres.setText(currentUser.getString("name"));
             if (currentUser.getDate("birthday") != null) {
-                lblFecha.setText(new SimpleDateFormat("dd/MM/yyyy").format(currentUser.getDate("birthday")));
+                txtFecha.setText(new SimpleDateFormat("dd/MM/yyyy").format(currentUser.getDate("birthday")));
+            }
+            radioGroup.clearCheck();
+            String gender = currentUser.getString("gender");
+            System.out.println("GENDER " + gender);
+            if (gender != null && gender.equals("F")) {
+                rbtnF.setChecked(true);
+            } else {
+                rbtnM.setChecked(true);
             }
         }
     }
@@ -132,7 +203,7 @@ public class WizardFirstFragment extends Fragment {
         public void onDateSet(DatePicker view, int year, int month, int day) {
             // Do something with the date chosen by the user
             String patter = "%02d/%02d/%d";
-            lblFecha.setText(String.format(patter, day, month + 1, year));
+            txtFecha.setText(String.format(patter, day, month + 1, year));
         }
 
     }
